0cbc932557c0c352cbc5debdc9b04243becd39f8
Darien A. Toh Wai Ken <darien@alfacom.com.my>
2018-10-23 15:19:18 +0800

    hardcoded deals image

--- src/pages/deals-modal/deals-modal.ts
+++ src/pages/deals-modal/deals-modal.ts
@@ -17,7 +17,7 @@ export class DealsModalPage  implements OnInit {
   @ViewChild('slides') slides: Slides;
   suggestions:any[] = [];
   selectedItem:any;
-  
+  imgRoot:string = "assets/imgs/deals/"
 
 
   public constructor(public viewCtrl: ViewController,public modalCtrl: ModalController,public navParams: NavParams) {
@@ -33,8 +33,16 @@ export class DealsModalPage  implements OnInit {
     
   }
 
-  selectItem(item){
+  getImgSrc(name:string){
+    console.log(name)
+    if (name || name == '0'){
+      return this.imgRoot + name.toString().toLowerCase() + '.jpg'
+    }
+  }
+
+  selectItem(item,idx){
     this.selectedItem = item;
+    this.selectedItem.index = idx;
     this.nextSlide();
   }
 
