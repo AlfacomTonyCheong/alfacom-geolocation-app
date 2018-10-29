efece7dcacc8da83d6dcdd9f340fdbf2b114405f
Tony Cheong <tcheong@alfacom.com.my>
2018-10-29 17:57:12 +0800

    UI changes

--- src/pages/credit/credit.ts
+++ src/pages/credit/credit.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the CreditPage page.
@@ -14,11 +14,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'credit.html',
 })
 export class CreditPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
   }
 
   ionViewDidEnter() {
   }
 
-  
+  dismiss(){
+    this.viewCtrl.dismiss();
+  }
 }
